5ac79b2f936ecdc7bcfc42732ac6a5d87991c5f4
JAguilar <101124609+JAlejandroAC@users.noreply.github.com>
2026-01-26 21:22:00 +0000

    add options variables

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -8,6 +8,7 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
+import click
 
 
 
@@ -40,18 +41,17 @@ parse_dates = [
 
 
 
-def run():
-    year = 2021
-    month = 1
-
-    pg_user = 'root'
-    pg_password = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    target_table = 'yellow_taxi_data'
-    chunk_size = 100000
-
+@click.command()
+@click.option('--year', default=2021, help='Year for the data ingestion')
+@click.option('--month', default=1, help='Month for the data ingestion')
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table for data ingestion')
+@click.option('--chunk-size', default=100000, help='Chunk size for data ingestion')
+def run(year, month, pg_user, pg_password, pg_host, pg_port, pg_db, target_table, chunk_size):
     engine = create_engine(f'postgresql://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
 # In[2]:
 
